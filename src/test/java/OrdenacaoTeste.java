301d07471e20a16a4b76b9d06e480e4d7f5efa9e
Jairo Santos jr <79456331+jairojunior955@users.noreply.github.com>
2022-03-17 14:39:23 -0300

    shell sort testes
    alunos: Jairo Júnior e Fabrício Silva

--- src/test/java/OrdenacaoTeste.java
+++ src/test/java/OrdenacaoTeste.java
@@ -142,5 +142,26 @@ public class OrdenacaoTeste {
         int[] retorno = Ordenacao.quickSort(vetor, 0, vetor.length-1);
         assertArrayEquals(new int[] {-9, -7, -4, -3, -1}, retorno);
     }
-
+    
+    //Shell Sort
+    @Test
+    public void testarNumerosIguaisShellSort() {
+        int[] vetor = {1, 1, 1, 1, 1};
+        int[] retorno = Ordenacao.shellSort(vetor);
+        assertArrayEquals(vetor, retorno);
+    }
+    
+    @Test
+    public void testarNumerosPositivosShellSort() {
+        int[] vetor = {7, 4, 8, 3, 9, 0, 2, 1, 5, 6};
+        int[] retorno = Ordenacao.shellSort(vetor);
+        assertArrayEquals(new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, retorno);
+    }
+    
+    @Test
+    public void testarNumerosNegativosShellSort() {
+        int[] vetor = {-9, -1, -4, -3, -7};
+        int[] retorno = Ordenacao.shellSort(vetor);
+        assertArrayEquals(new int[] {-9, -7, -4, -3, -1}, retorno);
+    }
 }
